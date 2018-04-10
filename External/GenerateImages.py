0141dfe168f9a261a963331b3b616729558f68a1
VRoom <jtkotlarek@ucdavis.edu>
2018-04-10 13:19:43 -0700

    continue work on GenerateImages.py

--- External/GenerateImages.py
+++ External/GenerateImages.py
@@ -16,7 +16,22 @@ XRange = max(vertex, key=lambda x:x['x'])['x'] - minX
 
 for v in vertex:
 	v['y'] = (v['y']-minY) / YRange
-	v['y'] = v['y']*0.8 + 0.1
+	v['y'] = v['y']*0.9 + 0.05
 	v['x'] = (v['x']-minX) / XRange
-	v['x'] = v['x']*0.8 + 0.1
+	v['x'] = v['x']*0.9 + 0.05
 
+current = [[]]
+index = 0
+timestep = 0
+with open('Data/xgca_mira044_jb_trapped_passing.dat') as f:
+	for line in f:
+		if line == "\n":
+			index = 0
+			timestep += 1
+			current.append([])
+		else:
+			[t, p] = line.split();
+			if index == 0:
+				current[timestep].append([])
+			current[timestep][index].append({'t':0,'p':0})
+		index += 1
