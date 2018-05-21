d9d8e9cd1e77758a58a9902f32d47b73866cb825
DESKTOP-VSCBR3L\Joe <jtkotlarek@gmail.com>
2018-05-21 00:41:02 -0700

    Add Cube Minimap

---
+++ Assets/Scripts/CubeMiniMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMiniMap : MonoBehaviour {
+
+    public Transform followPosition;
+    public Transform trackedPosition;
+    public Transform trackerPosition;
+    public Transform tokamakForScale;
+
+    Vector3 FollowPosition { get { return followPosition.position; } }
+    Vector3 TrackedPosition { get { return trackedPosition.position; } }
+    Vector3 TrackerPosition { get { return trackerPosition.localPosition; } set { trackerPosition.localPosition = value; } }
+    Vector3 TokamakPosition { get { return tokamakForScale.position; } }
+    Vector3 TokamakScale { get { return tokamakForScale.localScale; } }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, FollowPosition, 1.0f) + Vector3.up * 0.2f;
+        TrackerPosition = Vector3ComponentDivide(TrackedPosition - TokamakPosition, TokamakScale);
+	}
+
+    Vector3 Vector3ComponentDivide(Vector3 a, Vector3 b) {
+        return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+}
